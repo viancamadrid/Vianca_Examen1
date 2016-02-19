4195be40399bb1dfe60e3ad41e892c48e7d376a2
viancamadrid <viancamadrid@unitec.edu>
2016-02-19 16:05:12 -0600

    validaciones

--- examen1.cpp
+++ examen1.cpp
@@ -8,8 +8,9 @@ using std::endl;
 int** crearTablero();
 void llenarCubo(int**);
 void imprimirTablero(int**);
-int** puntos(int**, int, int, int, int);
+int** convertir(int**, int, int, int, int);
 bool validar(int**, int, int);
+bool lleno(int**);
 
 int main(int ragc, char*argv[]){
 	int** matriz;
@@ -20,7 +21,7 @@ int main(int ragc, char*argv[]){
 	bool pos;
 	while(ganador==false){
 		imprimirTablero(matriz);
-		ganador=true;
+		//ganador=true;
 		cout<<"---Juagdor 1---"<<endl;
 		cout<<"Posicion x: ";
 		cin>>p1;
@@ -28,9 +29,25 @@ int main(int ragc, char*argv[]){
 		cin>>p2;
 		pos=validar(matriz,p1,p2);		
 		if(pos){
-			
+			matriz=convertir(matriz, p1, p2, 2, 1);
+			imprimirTablero(matriz);		
 		}else{
 			cout<<"No se Puede Colocar en Esa Coordenada"<<endl;
+		}
+		if(ganador==false){
+			cout<<"---Juagdor 2---"<<endl;
+	                cout<<"Posicion x: ";
+        	        cin>>p1;
+                	cout<<"Posicion y: ";
+                	cin>>p2;
+			pos=validar(matriz,p1,p2);
+               		if(pos){
+                        	matriz=convertir(matriz, p1, p2, 1, 2);
+                        	imprimirTablero(matriz);
+               	 	}else{
+                        	cout<<"No se Puede Colocar en Esa Coordenada"<<endl;
+                	}
+
 		}
 				
 	}
@@ -67,9 +84,9 @@ void imprimirTablero(int** tablero){
 }
 
 int** convertir(int** matriz, int x, int y, int op, int jug){
-	int a1, a2, a3, a4, a5, a7;
 	int cont=1;
-	if((x!=0) || (y!=0) || (x!=6) || (y!=6){
+	matriz[x][y]=jug;
+	if((x!=0) || (y!=0) || (x!=6) || (y!=6)){
 		if(matriz[x-cont][y-cont]==op){
 			matriz[x-cont][y-cont]=jug;
 		}
@@ -94,7 +111,9 @@ int** convertir(int** matriz, int x, int y, int op, int jug){
 		if(matriz[x+cont][y+cont]==op){
 			matriz[x+cont][y+cont]=jug;
 		}
-	}
+	}else{
+		
+	}	
 	return matriz;
 	
 }
@@ -105,4 +124,19 @@ bool validar(int** matriz, int x, int y){
 		poner=false;
 	}
 	return poner;
+}
+
+bool lleno (int** matriz){
+	int c=0;
+	for(int i=0; i<7; i++){
+		for (int j=0, j<7; j++){
+			if(matriz[i][j]=0){
+				c++;
+			}	
+		}
+	}
+	if(c==0)
+		return true;
+	else
+		return false;
 }
